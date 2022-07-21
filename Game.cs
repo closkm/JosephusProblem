545a98b5e1658ee2cbff6aafd696fe5729dad8bd
Michael Closky <mikeclosky29@gmail.com>
2022-07-21 11:46:02 -0400

    Add project files.

---
+++ Game.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JosephusProblem
+{
+    internal class Game
+    {
+        private List<int> positions = new List<int>();
+        private List<string> printable = new List<string>();
+        private int killingInterval;
+        private int numberOfPeople;
+        int positionInCircle = 1;
+        int roundCounter = 1;
+
+        public Game(int killingInterval, int numberOfPeople)
+        {
+            this.killingInterval = killingInterval;
+            this.numberOfPeople = numberOfPeople;
+            for (int i = 0; i <= numberOfPeople -1; i++)
+            {
+                positions.Add(i + 1);
+            }
+        }
+
+        public void LastPersonStanding()
+        {
+            while(positions.Count > 1)
+            {
+                for (int i = 0; i < positions.Count; i++)
+                {
+                    if (positionInCircle % killingInterval == 0)
+                    {
+                        printable.Add($"({positions[i]})");
+                        positions.RemoveAt(i);
+                        positionInCircle++;
+                        i--;
+                    }
+                    else
+                    {
+                        printable.Add($"{positions[i]}");
+                        positionInCircle++;
+                    }
+                }
+                    Console.WriteLine($"Round {roundCounter}: {string.Join(" ", printable)}");
+                    printable.Clear();
+                    roundCounter++;
+            }
+                    Console.WriteLine($"The winner is {string.Join("", positions)} at round {roundCounter}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace JosephusProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game(3, 20);
+            game.LastPersonStanding();
+        }
+    }
+}
